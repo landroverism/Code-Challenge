1e02c90a9c97a8fea19db94be9afdceca4207edd
Ham kemboi <kemboiham3@gmail.com>
2023-06-22 11:18:03 +0300

    completed assignment

---
+++ Net salary Calculator/index.js
@@ -0,0 +1,46 @@
+if (isNaN(salary)) {
+  document.getElementById("net").innerHTML = "Enter a valid salary";
+  return;
+}
+
+// calculating the net salary of an employee earning below 0.
+if (salary < 0) {
+  document.getElementById("net").innerHTML = "Your net salary is 0";
+}
+
+// calculating the net salary of an employee earning below 24,000 using NHIF rate of 2.5% and NSSF rate of 6%.
+else if (salary < 24000) {
+  let tax = salary * 0.1;
+  let nssf = salary * 0.06;
+  let nhif = salary * 0.025;
+
+  let netSalary = salary - (tax + nssf + nhif);
+
+  document.getElementById("net").innerHTML = `Your net salary is ${netSalary.toFixed(2)}, PAYEE TAX: ${tax.toFixed(2)}, NHIF DEDUCTIONS: ${nhif.toFixed(2)}, NSSF DEDUCTIONS: ${nssf.toFixed(2)}`;
+}
+
+// calculating the net salary of an employee earning above 24,000 and below 32333 using NHIF rate of 2.5% and NSSF rate of 6%.
+else if (salary >= 24000 && salary < 32333) {
+  let tax = salary * 0.25;
+  let nssf = salary * 0.06;
+  let nhif = salary * 0.025;
+
+  let netSalary = salary - (tax + nssf + nhif);
+
+  document.getElementById("net").innerHTML = `Your net salary is ${netSalary.toFixed(2)}, PAYEE TAX: ${tax.toFixed(2)}, NHIF DEDUCTIONS: ${nhif.toFixed(2)}, NSSF DEDUCTIONS: ${nssf.toFixed(2)}`;
+}
+
+// calculating the net salary of an employee earning above 32333 using NHIF rate of 2.5% and NSSF rate of 6%.
+else if (salary > 32333) {
+  let tax = salary * 0.3;
+  let nssf = salary * 0.06;
+  let nhif = salary * 0.025;
+
+  let netSalary = salary - (tax + nssf + nhif);
+
+  document.getElementById("net").innerHTML = `Your net salary is ${netSalary.toFixed(2)}, PAYEE TAX: ${tax.toFixed(2)}, NHIF DEDUCTIONS: ${nhif.toFixed(2)}, NSSF DEDUCTIONS: ${nssf.toFixed(2)}`;
+}
+
+else {
+  document.getElementById("net").innerHTML = "Enter salary";
+}
